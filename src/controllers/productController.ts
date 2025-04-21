ffb0b07ff32a990e1fc85f0e4595b2b733f1389e
MaykonWSF <maykonwillyam038@gmail.com>
2025-04-21 17:33:56 -0300

    Adicionando funcionalidade de retornar produto por id

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,4 +1,4 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import { AuthenticatedRequest } from "../middlewares/authMiddleware";
 import { JwtPayload } from "jsonwebtoken";
@@ -36,3 +36,41 @@ export const cadastrarProduto = async (
     res.status(500).json({ message: "Erro ao cadastrar produto", error });
   }
 };
+
+export const getProductById = async (
+    req: Request<{ id: string }>,
+    res: Response
+  ): Promise<void> => {
+    const idProduto = parseInt(req.params.id);
+  
+    if (isNaN(idProduto)) {
+      res.status(400).json({ message: "ID inválido." });
+      return;
+    }
+  
+    try {
+      const produto = await prisma.produtos.findUnique({
+        where: { idProduto },
+        include: {
+          doador: {
+            select: {
+              idUsuario: true,
+              nome: true,
+              email: true,
+              nomeOrganizacao: true,
+            }
+          }
+        }
+      });
+  
+      if (!produto) {
+        res.status(404).json({ message: "Produto não encontrado." });
+        return;
+      }
+  
+      res.status(200).json({ produto });
+    } catch (error) {
+      console.error("Erro ao buscar produto:", error);
+      res.status(500).json({ message: "Erro ao buscar produto", error });
+    }
+  };
--- src/routes/procuctRoutes.ts
+++ src/routes/procuctRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { cadastrarProduto } from "../controllers/productController";
+import { cadastrarProduto, getProductById } from "../controllers/productController";
 import { upload } from "../middlewares/uploadImage";
 import { authenticateToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 router.post("/produto/cadastrar", authenticateToken, upload.single("imagem"), cadastrarProduto);
+router.get("/produto/:id", authenticateToken, getProductById);
 
 export default router;
